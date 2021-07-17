6777569bdf1d3a25ca775aee13ad157d0c5f5f64
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-07-17 22:43:09 +0530

    Add Level-2 security and environment variable.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const { urlencoded } = require("body-parser");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -16,10 +16,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true , useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -73,5 +76,5 @@ app.route("/register")
 
 
 app.listen(3000, function(){
-    console.log("Server started on posrt 3000.");
+    console.log("Server started on port 3000.");
 });
